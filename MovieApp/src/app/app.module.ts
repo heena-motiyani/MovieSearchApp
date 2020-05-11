3fcedae60809a74d497dd54519cb37329ee8df36
Heena <hmotiyani15@gmail.com>
2020-05-12 02:06:22 +0530

    Build Component Structure for the app

--- MovieApp/src/app/app.module.ts
+++ MovieApp/src/app/app.module.ts
@@ -3,10 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header.component';
+import { DisplayMovieComponent } from './display-movie.component';
+import { SearchComponent } from './search.component';
+import { MoviePreviewComponent } from './search/movie-preview/movie-preview.component';
+import { FooterComponent } from './footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,HeaderComponent, DisplayMovieComponent, SearchComponent, MoviePreviewComponent, FooterComponent
   ],
   imports: [
     BrowserModule,
